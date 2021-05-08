e91bf7c365bedb33d7d333d5e4afa63361ced145
Alexandru Marcu <alumarcu@gmail.com>
2021-05-09 00:14:40 +0200

    update web address

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // https://www.typescriptlang.org/docs/
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World!
+        <img alt="" className="my-avatar" src="https://avatars.githubusercontent.com/alumarcu" />
       </header>
     </div>
   );
